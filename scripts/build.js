f5793032c91350c90ba1f4e4b8f621381b1339e1
Blake Embrey <hello@blakeembrey.com>
2016-10-18 12:04:42 -0700

    Update CSS properties

--- scripts/build.js
+++ scripts/build.js
@@ -1,24 +1,32 @@
-var browserRun = require('browser-run')
+var run = require('browser-run')
 var fs = require('fs')
 var join = require('path').join
+var arrify = require('arrify')
+var minimist = require('minimist')
 
 var GET_STYLES = 'console.log(JSON.stringify(Array.prototype.slice.call(document.defaultView.getComputedStyle(document.body, ""))));window.close();'
 
 var VENDOR_DIR = join(__dirname, '../vendor')
 
-function run (browser) {
-  var b = browserRun({ browser: browser })
-  var w = fs.createWriteStream(join(VENDOR_DIR, browser + '.json'))
+function build (browser) {
+  var b = run({ browser: browser })
+  var f = fs.createWriteStream(join(VENDOR_DIR, browser + '.json'))
 
-  b.pipe(w)
-  b.write(GET_STYLES)
-  b.end()
+  b.pipe(f)
+  b.end(GET_STYLES)
 }
 
-;[
-  'phantom',
-  'chrome',
-  // 'ie',
-  // 'firefox', // Damn you Firefox, why you no work?
-  'safari'
-].map(run)
+var browsers = minimist(process.argv.slice(2), {
+  defaults: {
+    browser: [
+      'electron',
+      'phantom',
+      'chrome',
+      // 'ie',
+      // 'firefox',
+      'safari'
+    ]
+  }
+}).browser
+
+arrify(browsers).map(build)
